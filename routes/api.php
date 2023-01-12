92efdd9b329f2ccee780413b2e58bd0d5c91bd29
Balics Gábor <balicsgabor@gmail.com>
2023-01-12 13:38:06 +0100

    seed, routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'merchants'], function() {
+    Route::get('/get', [\App\Http\Controllers\MerchantController::class, 'index']);
+    Route::post('/create', [\App\Http\Controllers\MerchantController::class, 'create']);
+    Route::put('/update/{merchant}', [\App\Http\Controllers\MerchantController::class, 'update']);
+    Route::delete('/delete/{merchant}', [\App\Http\Controllers\MerchantController::class, 'delete']);
+});
+
+Route::group(['prefix' => 'brands'], function() {
+    Route::get('/get', [\App\Http\Controllers\BrandController::class, 'index']);
+    Route::post('/create', [\App\Http\Controllers\BrandController::class, 'create']);
+    Route::put('/update/{brand}', [\App\Http\Controllers\BrandController::class, 'update']);
+    Route::delete('/delete/{brand}', [\App\Http\Controllers\BrandController::class, 'delete']);
+});
+
+Route::group(['prefix' => 'models'], function() {
+    Route::get('/get', [\App\Http\Controllers\ModelController::class, 'index']);
+    Route::post('/create', [\App\Http\Controllers\ModelController::class, 'create']);
+    Route::put('/update/{model}', [\App\Http\Controllers\ModelController::class, 'update']);
+    Route::delete('/delete/{model}', [\App\Http\Controllers\ModelController::class, 'delete']);
+});
+
+Route::group(['prefix' => 'cars'], function() {
+    Route::get('/get', [\App\Http\Controllers\CarController::class, 'index']);
+    Route::post('/create', [\App\Http\Controllers\CarController::class, 'create']);
+    Route::put('/update/{car}', [\App\Http\Controllers\CarController::class, 'update']);
+    Route::delete('/delete/{car}', [\App\Http\Controllers\CarController::class, 'delete']);
 });
